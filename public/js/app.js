944b5d68760cfea7481105104c40f679a3588192
Kira Hartlage <kira.hartlage@gmail.com>
2018-03-16 09:34:47 -0400

    can prepopulate checkboxes on days but they are not in order

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,3 @@
-function updateMeal(id) {
-  console.log("I am printing ", id);
+$(document).ready({
 
-  const mealData = {
-    timeOfDay: $('select[name=timeOfDay]').val(),
-    mealName: $('input[name=mealName]').val(),
-    notes: $('textarea[name=notes]').val(),
-    // categories:
-    // days:
-  };
-
-  let categories = $('input:checkbox[name="categories"]:checked').map(() => {
-    return $(this).val();
-  }).get();
-  console.log(categories);
-
-  // $.ajax({
-  //   type: 'PUT',
-  //   url: '/meal/' + id,
-  //   data: JSON.stringify(mealData),
-  //   dataType: 'json',
-  //   contentType: 'application/json'
-  // })
-  //   .done(function(response) {
-  //     console.log("We have posted the data");
-  //   })
-  //   .fail(function(error) {
-  //     console.log("Failed to post");
-  //   })
-
-    console.log("Your meal data", mealData);
-};
+});
