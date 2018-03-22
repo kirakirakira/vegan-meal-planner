c674edfb54474087d483fa2aca726bdf1b645705
Kira Hartlage <kira.hartlage@gmail.com>
2018-03-22 19:32:30 -0400

    added delete button to weekly view

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,13 @@
 
 function handleDeleteFileClick(id, day) {
-  if (confirm("Are you sure? ")) {
+  if (confirm("Are you sure?")) {
     deleteFile(id, day);
+  }
+}
+
+function handleDeleteMealClickWeek(id) {
+  if (confirm("Are you sure?")) {
+    deleteMealWeek(id);
   }
 }
 
@@ -20,6 +26,22 @@ function deleteFile(id, day) {
       console.log("Meal", id, "is deleted.");
       $("#dayOfWeek").text(day);
       $("#tableData").load('/' + day);
+    })
+    .fail(function(error) {
+      console.log("Failed to delete", error);
+    })
+}
+
+function deleteMealWeek(id) {
+  $.ajax({
+    type: 'DELETE',
+    url: '/meal/delete/' + id,
+    dataType: 'json',
+    contentType : 'application/json',
+  })
+    .done(function(response) {
+      console.log("Meal", id, "is deleted.");
+      $("#tableDataWeek").load('/');
     })
     .fail(function(error) {
       console.log("Failed to delete", error);
